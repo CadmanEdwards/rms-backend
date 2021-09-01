7fab576a74fbcc19e102e1ab6d46ae4fa813f22a
francisgill42 <francisgill42@gmail.com>
2021-09-01 16:03:51 -0700

    fixed

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -13,7 +13,7 @@ class Role extends Model
 
     public function permissions()
     {
-        return $this->hasMany(permission::class);
+        return $this->hasMany(Permission::class);
     }
 
     
