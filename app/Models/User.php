76f4b82b107589145bc552fe68316ded4dfc9698
ord0077 <ord0077@gmail.com>
2021-09-06 23:19:50 +0500

    Dev1 forget and reset password

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordNotification;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -51,5 +52,12 @@ class User extends Authenticatable
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = \Hash::make($value);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $url= 'http://127.0.0.1:8000/api/forget-password' . $token;
+
+        $this->notify(new ResetPasswordNotification($url));
     }
 }
